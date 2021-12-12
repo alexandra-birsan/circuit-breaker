26c1faef0a1f744a046cede3eb56f4fb58d4a165
alexandra-birsan <birsan.dev@gmail.com>
2021-12-12 13:32:23 +0200

    Example of Resilience4j circuit breaker

--- src/main/java/com/example/circuit/breaker/controller/CircuitBreakerController.java
+++ src/main/java/com/example/circuit/breaker/controller/CircuitBreakerController.java
@@ -1,14 +1,20 @@
 package com.example.circuit.breaker.controller;
 
+import com.example.circuit.breaker.service.AlbumService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 public class CircuitBreakerController {
 
-  @GetMapping(value = "/hello")
-  private Mono<String> sayHello() {
-    return Mono.just("Hi, I'm Alex!");
-  }
+    private final AlbumService albumService;
+
+    public CircuitBreakerController(AlbumService albumService) {
+        this.albumService = albumService;
+    }
+
+    @GetMapping(value = "/albums")
+    private String getAlbums() {
+        return albumService.getAlbums();
+    }
 }
